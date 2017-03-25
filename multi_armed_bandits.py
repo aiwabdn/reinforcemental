7913aefbe5ebc8bebdc8a0def79019fe7e18b000
Anindya Basu <aiwabdn@anindyas-mbp.home>
2017-03-25 10:42:08 +0000

    added legend to plot. cleaned up comments.

--- multi_armed_bandits.py
+++ multi_armed_bandits.py
@@ -16,14 +16,13 @@ cum_reward = np.zeros((3,timesteps))
 
 # at each timestep select the arm with max rewards and update r_mean
 for e in range(len(eps)):
+    # reset counts of selection and expected rewards
     r_select_counts = np.zeros((sz_testbed,sz_bandit))
     r_mean = np.zeros((sz_testbed, sz_bandit))
     for i in range(timesteps):
-        #print(i)
         for ix,row in enumerate(r_mean):
             #select arm
             arm = np.argmax(row) if np.random.uniform() <= (1 - eps[e]) else np.random.choice(np.arange(sz_bandit))
-            #print('selected arm at step {} : {}'.format(i+1, arm))
             # update arm selection count
             r_select_counts[ix,arm] += 1
             #get reward for that arm
@@ -31,14 +30,14 @@ for e in range(len(eps)):
             cum_reward[e,i] += R_t
             #update mean reward for that arm
             row[arm] += (R_t - row[arm])/r_select_counts[ix,arm]
-            #print('reward : ', R_t)
-    print(cum_reward)
 
 print(r_select_counts)
 print(cum_reward)
 
 # plot average rewards over time
-plt.plot(cum_reward[0]/2000, 'b', cum_reward[1]/2000, 'g', cum_reward[2]/2000, 'r')
+for e in range(len(eps)):
+    plt.plot(cum_reward[e]/sz_testbed, label = 'e='+str(eps[e]))
+plt.legend()
 plt.show()
 
 # the learning method with the lowest e-greedy learns slowly but constantly grows over time.
